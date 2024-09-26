d27f50a075c52163f8f1080918a0e6e7e49a0a91
Romen Poirier Taksev <romenpt@hotmail.com>
2024-09-26 18:07:01 -0400

    changed .sh

--- p1-students/A1/Template/Server/run_middleware.sh
+++ p1-students/A1/Template/Server/run_middleware.sh
@@ -6,4 +6,4 @@ echo '  $2 - hostname of Cars'
 echo '  $3 - hostname of Rooms'
 echo '  $3 - hostname of Customers'
 
-java -Djava.rmi.server.codebase=file:$(pwd)/ Server.RMI.RMIMiddleware $1 $2 $3 $4
+java -Djava.rmi.server.codebase=file:$(pwd)/ Server.RMI.RMIMiddleware $1 $2 $3 $4 $5
