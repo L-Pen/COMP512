4980de38f9256114f185d2eacd9cf2c617b0c808
Liamo Pennimpede <liamo@Liamos-MacBook-Pro-2.local>
2024-09-28 12:07:32 -0400

    adding input messages

--- p1-students/A1/Template/Client/Client/RMIClient.java
+++ p1-students/A1/Template/Client/Client/RMIClient.java
@@ -67,6 +67,7 @@ public class RMIClient extends Client {
 				Command cmd = null;
 				try{
 					cmd = Command.fromString((String) arguments.elementAt(0));
+					printInputs(cmd, arguments);
 				} catch (IllegalArgumentException e) {
 					System.out.println("Invalid command");
 					continue;
@@ -74,6 +75,8 @@ public class RMIClient extends Client {
 				
 				if (cmd.equals(Command.Quit))
 					break;
+
+				
 
 				String commandString = cmd.name() + ",";
 				for (int i = 1; i < arguments.size(); i++) {
@@ -123,4 +126,192 @@ public class RMIClient extends Client {
 			System.exit(1);
 		}
 	}
+
+	public static void printInputs(Command cmd,Vector<String> arguments ) {
+		switch (cmd) {
+			case Help: {
+				if (arguments.size() == 1) {
+					System.out.println(Command.description());
+				} else if (arguments.size() == 2) {
+					Command l_cmd = Command.fromString((String) arguments.elementAt(1));
+					System.out.println(l_cmd.toString());
+				} else {
+					System.err.println((char) 27 + "[31;1mCommand exception: " + (char) 27
+							+ "[0mImproper use of help command. Location \"help\" or \"help,<CommandName>\"");
+				}
+				break;
+			}
+			case AddFlight: {
+				checkArgumentsCount(4, arguments.size());
+
+				System.out.println("Adding a new flight ");
+				System.out.println("-Flight Number: " + arguments.elementAt(1));
+				System.out.println("-Flight Seats: " + arguments.elementAt(2));
+				System.out.println("-Flight Price: " + arguments.elementAt(3));
+				break;
+			}
+			case AddCars: {
+				checkArgumentsCount(4, arguments.size());
+
+				System.out.println("Adding new cars");
+				System.out.println("-Car Location: " + arguments.elementAt(1));
+				System.out.println("-Number of Cars: " + arguments.elementAt(2));
+				System.out.println("-Car Price: " + arguments.elementAt(3));
+				break;
+			}
+			case AddRooms: {
+				checkArgumentsCount(4, arguments.size());
+
+				System.out.println("Adding new rooms");
+				System.out.println("-Room Location: " + arguments.elementAt(1));
+				System.out.println("-Number of Rooms: " + arguments.elementAt(2));
+				System.out.println("-Room Price: " + arguments.elementAt(3));
+				break;
+			}
+			case AddCustomer: {
+				checkArgumentsCount(1, arguments.size());
+
+				System.out.println("Adding a new customer");
+				break;
+			}
+			case AddCustomerID: {
+				checkArgumentsCount(2, arguments.size());
+
+				System.out.println("Adding a new customer");
+				System.out.println("-Customer ID: " + arguments.elementAt(1));
+				break;
+			}
+			case DeleteFlight: {
+				checkArgumentsCount(2, arguments.size());
+
+				System.out.println("Deleting a flight");
+				System.out.println("-Flight Number: " + arguments.elementAt(1));
+				break;
+			}
+			case DeleteCars: {
+				checkArgumentsCount(2, arguments.size());
+
+				System.out.println("Deleting all cars at a particular location");
+				System.out.println("-Car Location: " + arguments.elementAt(1));
+				break;
+			}
+			case DeleteRooms: {
+				checkArgumentsCount(2, arguments.size());
+
+				System.out.println("Deleting all rooms at a particular location");
+				System.out.println("-Car Location: " + arguments.elementAt(1));
+				break;
+			}
+			case DeleteCustomer: {
+				checkArgumentsCount(2, arguments.size());
+
+				System.out.println("Deleting a customer from the database");
+				System.out.println("-Customer ID: " + arguments.elementAt(1));
+				break;
+			}
+			case QueryFlight: {
+				checkArgumentsCount(2, arguments.size());
+
+				System.out.println("Querying a flight");
+				System.out.println("-Flight Number: " + arguments.elementAt(1));
+				break;
+			}
+			case QueryCars: {
+				checkArgumentsCount(2, arguments.size());
+
+				System.out.println("Querying cars location");
+				System.out.println("-Car Location: " + arguments.elementAt(1));
+				break;
+			}
+			case QueryRooms: {
+				checkArgumentsCount(2, arguments.size());
+
+				System.out.println("Querying rooms location");
+				System.out.println("-Room Location: " + arguments.elementAt(1));
+				break;
+			}
+			case QueryCustomer: {
+				checkArgumentsCount(2, arguments.size());
+
+				System.out.println("Querying customer information");
+				System.out.println("-Customer ID: " + arguments.elementAt(1));
+				break;
+			}
+			case QueryFlightPrice: {
+				checkArgumentsCount(2, arguments.size());
+
+				System.out.println("Querying a flight price");
+				System.out.println("-Flight Number: " + arguments.elementAt(1));
+				break;
+			}
+			case QueryCarsPrice: {
+				checkArgumentsCount(2, arguments.size());
+
+				System.out.println("Querying cars price");
+				System.out.println("-Car Location: " + arguments.elementAt(1));
+				break;
+			}
+			case QueryRoomsPrice: {
+				checkArgumentsCount(2, arguments.size());
+
+				System.out.println("Querying rooms price");
+				System.out.println("-Room Location: " + arguments.elementAt(1));
+				break;
+			}
+			case ReserveFlight: {
+				checkArgumentsCount(3, arguments.size());
+
+				System.out.println("Reserving seat in a flight");
+				System.out.println("-Customer ID: " + arguments.elementAt(1));
+				System.out.println("-Flight Number: " + arguments.elementAt(2));
+				break;
+			}
+			case ReserveCar: {
+				checkArgumentsCount(3, arguments.size());
+
+				System.out.println("Reserving a car at a location");
+				System.out.println("-Customer ID: " + arguments.elementAt(1));
+				System.out.println("-Car Location: " + arguments.elementAt(2));
+
+				int customerID = toInt(arguments.elementAt(1));
+				String location = arguments.elementAt(2);
+				break;
+			}
+			case ReserveRoom: {
+				checkArgumentsCount(3, arguments.size());
+
+				System.out.println("Reserving a room at a location");
+				System.out.println("-Customer ID: " + arguments.elementAt(1));
+				System.out.println("-Room Location: " + arguments.elementAt(2));
+
+				int customerID = toInt(arguments.elementAt(1));
+				String location = arguments.elementAt(2);
+				break;
+			}
+			case Bundle: {
+				if (arguments.size() < 6) {
+					System.err.println((char) 27 + "[31;1mCommand exception: " + (char) 27
+							+ "[0mBundle command expects at least 6 arguments. Location \"help\" or \"help,<CommandName>\"");
+					break;
+				}
+
+				System.out.println("Reserving an bundle");
+				System.out.println("-Customer ID: " + arguments.elementAt(1));
+				for (int i = 0; i < arguments.size() - 5; ++i) {
+					System.out.println("-Flight Number: " + arguments.elementAt(2 + i));
+				}
+				System.out.println("-Location for Car/Room: " + arguments.elementAt(arguments.size() - 3));
+				System.out.println("-Book Car: " + arguments.elementAt(arguments.size() - 2));
+				System.out.println("-Book Room: " + arguments.elementAt(arguments.size() - 1));
+
+				break;
+			}
+			case Quit:
+				checkArgumentsCount(1, arguments.size());
+
+				System.out.println("Quitting client");
+				System.exit(0);
+		}
+	}
 }
+
