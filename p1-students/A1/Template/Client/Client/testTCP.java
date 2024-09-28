00ed8125b8b27a55c561a4c9ef3b9c7eafc0224a
Liamo Pennimpede <liamo@Liamos-MacBook-Pro-2.local>
2024-09-28 13:26:42 -0400

    get rid of junit

---
+++ p1-students/A1/Template/Client/Client/testTCP.java
@@ -0,0 +1,111 @@
+package Client;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.BeforeEach;
+import org.junit.Test;
+import Client.RMIClient;
+
+
+public class testTCP {
+
+    private testRMI client;
+
+    @BeforeEach
+    public void setUp() {
+        RMIClient client = new RMIClient();
+    }
+
+    // @AfterEach
+    // public void tearDown() {
+    //     // cleanup logic after each test
+    // }
+
+    @Test
+    public void testAddFlight() {
+    }
+
+    @Test
+    public void testAddCars() {
+    }
+
+    @Test
+    public void testAddRooms() {
+    }
+
+    @Test
+    public void testAddCustomer() {
+
+    }
+
+    @Test
+    public void testAddCustomerID() {
+    }
+
+    @Test
+    public void testDeleteFlight() {
+    }
+
+    @Test
+    public void testDeleteCars() {
+    }
+
+    @Test
+    public void testDeleteRooms() {
+    }
+
+    @Test
+    public void testDeleteCustomer() {
+    }
+
+    @Test
+    public void testQueryFlight() {
+
+    }
+
+    @Test
+    public void testQueryCars() {
+
+    }
+
+    @Test
+    public void testQueryRooms() {
+
+    }
+
+    @Test
+    public void testQueryCustomer() {
+
+    }
+
+    @Test
+    public void testQueryFlightPrice() {
+
+    }
+
+    @Test
+    public void testQueryCarsPrice() {
+
+    }
+
+    @Test
+    public void testQueryRoomsPrice() {
+ 
+    }
+
+    @Test
+    public void testReserveFlight() {
+    }
+
+    @Test
+    public void testReserveCar() {
+    }
+
+    @Test
+    public void testReserveRoom() {
+    }
+
+    @Test
+    public void testBundle() {
+    }
+}
