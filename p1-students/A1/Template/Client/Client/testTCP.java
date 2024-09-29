6bc7db77810af6007269c6f4e8b4b560c7556c5b
Liamo Pennimpede <liamo@Liamos-MacBook-Pro-2.local>
2024-09-29 15:01:05 -0400

    trying with two clients

--- p1-students/A1/Template/Client/Client/testTCP.java
+++ p1-students/A1/Template/Client/Client/testTCP.java
@@ -11,7 +11,8 @@ public class testTCP {
     public static void testAddFlight() throws UnknownHostException, IOException, InterruptedException {
         String commandString = "AddFlight,123,456,789";
         String response = RMIClient.sendTcpRequest(9034, commandString);
-        System.out.println("Response from server: " + response);
+        String response2 = RMIClient.sendTcpRequest(9034, commandString);
+        System.out.println("Response from server: " + response + "," + response2);
     }
 
     public static void main(String[] args) throws UnknownHostException, IOException, InterruptedException {
