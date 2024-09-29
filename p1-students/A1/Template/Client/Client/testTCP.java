de8948832a7ff35a6a5503d4fbb639952d5866c1
Liamo Pennimpede <liamo@Liamos-MacBook-Pro-2.local>
2024-09-29 15:04:26 -0400

    adding first test

--- p1-students/A1/Template/Client/Client/testTCP.java
+++ p1-students/A1/Template/Client/Client/testTCP.java
@@ -9,10 +9,17 @@ import Client.RMIClient;
 public class testTCP {
 
     public static void testAddFlight() throws UnknownHostException, IOException, InterruptedException {
-        String commandString = "AddFlight,123,456,789";
-        String response = RMIClient.sendTcpRequest(9034, commandString);
-        String response2 = RMIClient.sendTcpRequest(9034, commandString);
-        System.out.println("Response from server: " + response + "," + response2);
+        String commandString1 = "AddFlight,123,100,789";
+        String commandString2 = "AddFlight,123,200,789";
+        String commandString3 = "queryFlight,123";
+        String response = RMIClient.sendTcpRequest(9034, commandString1);
+        String response2 = RMIClient.sendTcpRequest(9034, commandString2);
+        String response3 = RMIClient.sendTcpRequest(9034, commandString3);
+        if (response3.equals("300")) {
+            System.out.println("Test Passed");
+        } else {
+            System.out.println("Test Failed");
+        }
     }
 
     public static void main(String[] args) throws UnknownHostException, IOException, InterruptedException {
