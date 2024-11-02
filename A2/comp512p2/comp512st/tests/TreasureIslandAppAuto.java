c6301cde057d3420717469095f000db65c92eae7
Liamo Pennimpede <liamo@Liamos-MacBook-Pro-2.local>
2024-11-02 11:47:57 -0400

    increasing join thread time

--- A2/comp512p2/comp512st/tests/TreasureIslandAppAuto.java
+++ A2/comp512p2/comp512st/tests/TreasureIslandAppAuto.java
@@ -271,7 +271,7 @@ public class TreasureIslandAppAuto implements Runnable
 																							// May have to increase this for higher maxmoves and smaller intervals.
 		try{ Thread.sleep(5000); } catch (InterruptedException ie) { logger.log(Level.SEVERE, "I got InterruptedException when I was chilling after all my moves.", ie); }
 		ta.keepExploring = false;
-		ta.tiThread.join(1000); // Wait maximum 1s for the app to process any more incomming messages that was in the queue.
+		ta.tiThread.join(5000); // Wait maximum 1s for the app to process any more incomming messages that was in the queue.
 		logger.info("Shutting down Paxos");
 		paxos.shutdownPaxos(); // shutdown paxos.
 		ta.tiThread.interrupt(); // interrupt the app thread if it has not terminated.
