7fb98c0a83a58b9c33cd129bd836c656cc85f854
Romen Poirier Taksev <romenpt@hotmail.com>
2024-11-23 17:42:56 -0500

    progress

--- A3/dist/DistProcess.java
+++ A3/dist/DistProcess.java
@@ -21,34 +21,26 @@ import org.apache.zookeeper.data.*;
 
 import java.lang.Thread;
 
-// You may have to add other interfaces such as for threading, etc., as needed.
-// This class will contain the logic for both your manager process as well as the worker processes.
-//  Make sure that the callbacks and watch do not conflict between your manager's logic and worker's logic.
-//		This is important as both the manager and worker may need same kind of callbacks and could result
-//			with the same callback functions.
-//	For simplicity, so far all the code in a single class (including the callbacks).
-//		You are free to break it apart into multiple classes, if that is your programming style or helps
-//		you manage the code more modularly.
-//	REMEMBER !! Managers and Workers are also clients of ZK and the ZK client library is single thread - Watches & CallBacks should not be used for time consuming tasks.
-//		In particular, if the process is a worker, Watches & CallBacks should only be used to assign the "work" to a separate thread inside your program.
 public class DistProcess implements Watcher, AsyncCallback.ChildrenCallback, AsyncCallback.DataCallback, Runnable {
 	ZooKeeper zk;
 	String zkServer, pinfo;
 	volatile boolean isManager = false;
 	boolean initialized = false;
+	String hostname;
 
 	volatile private HashMap<String, String> workers = new HashMap<>();
 
 	volatile private HashMap<String, Boolean> taskStatus = new HashMap<>();
 	volatile private Queue<String> taskQueue = new LinkedList<>();
-	volatile private Object taskLock = new Object();
 
 	volatile private DistTask taskObject = null;
 	volatile private String workerTaskId = null;
 
+	volatile private Object taskLock = new Object();
+
 	DistProcess(String zkhost) {
 		zkServer = zkhost;
-		pinfo = ManagementFactory.getRuntimeMXBean().getName();
+		pinfo = ManagementFactory.getRuntimeMXBean().getName().split("@")[1];
 		System.out.println("DISTAPP : ZK Connection information : " + zkServer);
 		System.out.println("DISTAPP : Process information : " + pinfo);
 	}
@@ -75,7 +67,7 @@ public class DistProcess implements Watcher, AsyncCallback.ChildrenCallback, Asy
 							String taskId = taskQueue.remove();
 							System.out.println("==== Removed task from queue, length : " + taskQueue.size());
 							workers.put(workerId, taskId);
-							System.out.println(convertWithIteration(workers));
+							System.out.println(stringifyMap(workers));
 							try {
 								String path = "/dist30/workers/" + workerId + "/" + taskId;
 								zk.create(path, taskId.getBytes(),
@@ -186,14 +178,6 @@ public class DistProcess implements Watcher, AsyncCallback.ChildrenCallback, Asy
 	public void process(WatchedEvent e) {
 		// Get watcher notifications.
 
-		// !! IMPORTANT !!
-		// Do not perform any time consuming/waiting steps here
-		// including in other functions called from here.
-		// Your will be essentially holding up ZK client library
-		// thread and you will not get other notifications.
-		// Instead include another thread in your program logic that
-		// does the time consuming "work" and notify that thread from here.
-
 		// System.out.println("DISTAPP : Event received : " + e);
 
 		if (e.getType() == Watcher.Event.EventType.None) // This seems to be the event type associated with connections.
@@ -208,16 +192,15 @@ public class DistProcess implements Watcher, AsyncCallback.ChildrenCallback, Asy
 
 		// Manager should be notified if any new znodes are added to tasks.
 		if (e.getType() == Watcher.Event.EventType.NodeChildrenChanged && e.getPath().equals("/dist30/tasks")) {
-			// There has been changes to the children of the node.
-			// We are going to re-install the Watch as well as request for the list of the
-			// children.
 			getTasks();
 		}
 
+		// Manager should be notified if any new znodes are added to workers.
 		if (e.getType() == Watcher.Event.EventType.NodeChildrenChanged && e.getPath().equals("/dist30/workers")) {
 			getWorkers();
 		}
 
+		// Workers are notified when job is added to their znode
 		if (e.getType() == Watcher.Event.EventType.NodeChildrenChanged
 				&& e.getPath().equals("/dist30/workers/" + pinfo)) {
 			checkForTask();
@@ -228,7 +211,7 @@ public class DistProcess implements Watcher, AsyncCallback.ChildrenCallback, Asy
 			synchronized (workers) {
 				String taskId = workers.get(workerId);
 				workers.put(workerId, null);
-				System.out.println(convertWithIteration(workers));
+				System.out.println(stringifyMap(workers));
 				synchronized (taskLock) {
 					taskStatus.remove(taskId);
 				}
@@ -238,34 +221,19 @@ public class DistProcess implements Watcher, AsyncCallback.ChildrenCallback, Asy
 
 	// Asynchronous callback that is invoked by the zk.getChildren request.
 	public void processResult(int rc, String path, Object ctx, List<String> children) {
-
-		// !! IMPORTANT !!
-		// Do not perform any time consuming/waiting steps here
-		// including in other functions called from here.
-		// Your will be essentially holding up ZK client library
-		// thread and you will not get other notifications.
-		// Instead include another thread in your program logic that
-		// does the time consuming "work" and notify that thread from here.
-
-		// This logic is for manager !!
-		// Every time a new task znode is created by the client, this will be invoked.
-
-		// Also have a mechanism to assign these tasks to a "Worker" process.
-		// The worker must invoke the "compute" function of the Task send by the client.
-		// What to do if you do not have a free worker process?
 		// System.out.println("DISTAPP : processResult : " + rc + ":" + path + ":" +
 		// ctx);
 
-		if (path.equals("/dist30/workers")) {
+		if (isManager && path.equals("/dist30/workers")) {
 			synchronized (workers) {
 				for (String c : children) {
 					if (workers.containsKey(c))
 						continue;
 					workers.put(c, null);
 				}
-				System.out.println(convertWithIteration(workers));
+				System.out.println(stringifyMap(workers));
 			}
-		} else if (path.equals("/dist30/tasks")) {
+		} else if (isManager && path.equals("/dist30/tasks")) {
 			synchronized (taskLock) {
 				for (String c : children) {
 					if (taskStatus.containsKey(c))
@@ -275,8 +243,7 @@ public class DistProcess implements Watcher, AsyncCallback.ChildrenCallback, Asy
 					System.out.println("==== Added task to queue, length : " + taskQueue.size());
 				}
 			}
-		}
-		if (path.equals("/dist30/workers/" + pinfo)) {
+		} else if (!isManager && path.equals("/dist30/workers/" + pinfo)) {
 			if (children.size() == 0)
 				return;
 			synchronized (taskLock) {
@@ -307,7 +274,7 @@ public class DistProcess implements Watcher, AsyncCallback.ChildrenCallback, Asy
 		}
 	}
 
-	private String convertWithIteration(HashMap<?, ?> map) {
+	private String stringifyMap(HashMap<?, ?> map) {
 		StringBuilder mapAsString = new StringBuilder("{");
 		for (Object key : map.keySet()) {
 			mapAsString.append(key + "=" + String.valueOf(map.get(key)) + ", ");
@@ -322,8 +289,6 @@ public class DistProcess implements Watcher, AsyncCallback.ChildrenCallback, Asy
 		DistProcess dt = new DistProcess(System.getenv("ZKSERVER"));
 		dt.startProcess();
 
-		// Replace this with an approach that will make sure that the process is up and
-		// running forever.
 		Thread dtThread = new Thread(dt);
 		dtThread.start();
 	}
